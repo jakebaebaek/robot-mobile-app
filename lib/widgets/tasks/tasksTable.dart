55e8eab691f0cdd01afc67002e6e6fb18e610628
jake <jacobaek0515@daum.net>
2024-05-03 18:50:06 +0900

    first commit

--- lib/widgets/tasks/tasksTable.dart
+++ lib/widgets/tasks/tasksTable.dart
@@ -1,126 +1,70 @@
 import 'package:flutter/material.dart';
 
-class TasksTable extends StatelessWidget{
-  final List headersData;
-  final List<List> rowsData;
+class PointsTableWidget extends StatefulWidget {
+  PointsTableWidget({Key? key}) : super(key: key);
 
-  const TasksTable({
-    required this.headersData,
-    required this.rowsData,
-    super.key,});
-  // TasksTable({super.key, required asdf, required fdsa});
+  @override
+  _PointsTableWidgetState createState() => _PointsTableWidgetState();
+}
 
-  TasksRow makeRow(List rData, 
-                  [Alignment alignment = Alignment.center, 
-                  double? height,
-                  ]){
-    List<Widget> row = [];
-    for(String rd in rData){
-      row.add(
-        TasksCell(
-          alignment: alignment, 
-          child: Text(rd),
-          )
-        );
-    }
-    return TasksRow(
-      children: row
-    );
-  }
+class _PointsTableWidgetState extends State<PointsTableWidget> {
+  List<String> points = List.generate(4, (index) => 'POINT_$index')..add('+');
+  int? selectedRowIndex;
 
-  List<TasksRow> makeRows(List<List> rsData, 
-                  {Alignment alignment = Alignment.center, 
-                  double? height,
-                  }){
-    List<TasksRow> result = [];
-    for(List rsd in rsData){
-      result.add(makeRow(rsd, alignment, height));
-    }
-    return result;
+  void addPoint() {
+    setState(() {
+      int newIndex = points.length - 1;  // '+'를 제외한 새 인덱스
+      points.insert(newIndex, 'POINT_$newIndex');
+    });
   }
 
   @override
-  Widget build(BuildContext context){
-    return Column(
-      children: [
-        Table(
-          children: [makeRow(headersData)],
-        ),
-        Flexible(
-          child: SingleChildScrollView(
-            child: Table(
-              children: makeRows(rowsData, alignment: Alignment.center),
-            ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(  // 스크롤 가능하게 만들기
+        scrollDirection: Axis.vertical,  // 세로 방향 스크롤
+        child: Table(
+          columnWidths: const <int, TableColumnWidth>{
+            0: FlexColumnWidth(),  // 열 너비를 유연하게 조절
+          },
+          border: TableBorder.all(color: Colors.black),  // 모든 셀에 테두리 추가
+          children: List<TableRow>.generate(
+            points.length,
+                (index) {
+              return TableRow(
+                decoration: BoxDecoration(
+                  color: selectedRowIndex == index ? Colors.blue : Colors.transparent,  // 선택된 행의 색상 변경
+                ),
+                children: [
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,  // 세로 중앙 정렬
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (points[index] == '+') {
+                            addPoint();
+                          } else {
+                            selectedRowIndex = index;  // 선택된 행 인덱스 갱신
+                          }
+                        });
+                      },
+                      child: Container(
+                        height: 50,  // 셀 높이 지정
+                        alignment: Alignment.center,  // 텍스트 중앙 정렬
+                        child: Text(points[index], style: TextStyle(fontSize: 16)),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
         ),
-      ],
-    );
-  }
-}
-
-
-
-class TasksRow extends TableRow{
-  
-  Widget makeCell(List data){
-    return Container();
-  }
-  const TasksRow({
-    LocalKey? key,
-    Decoration? decoration,
-    required super.children
-  });
-
-  List<Widget> build(BuildContext context){
-    return children;
-  }
-}
-
-
-
-class TasksCell extends Container{
-  final Color? color;
-  final Border? border;
-  final BorderRadius? borderRadius;
-  final List<BoxShadow>? boxShadow;
-  final Gradient? gradient;
-  final BlendMode? backgroundBlendMode;
-  final BoxShape shape;
-  final Alignment alignment;
-  final double? height;
-  final double? width;
-
-  TasksCell({
-    Key? key,
-    this.color,
-    this.border = const Border(bottom: BorderSide(width: 0.5, color: Color.fromARGB(255,189,189,189))),
-    this.borderRadius,
-    this.boxShadow,
-    this.gradient,
-    this.backgroundBlendMode,
-    this.shape = BoxShape.rectangle,
-    this.alignment = Alignment.center,
-    this.height,
-    this.width,
-    required super.child,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context){
-    return Container(
-      decoration: BoxDecoration(
-      color: color,
-      border: border,
-      borderRadius: borderRadius,
-      boxShadow: boxShadow,
-      gradient: gradient,
-      backgroundBlendMode: backgroundBlendMode,
-      shape: shape,
       ),
-      height: height ?? 40,
-      width: width,
-      alignment: alignment,
-      child: child,
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(home: PointsTableWidget()));
 }
